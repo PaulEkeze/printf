284c69cc34776aae7b8976c5b6f2b5b0346362c7
attabadukwabena <attabadukwabena@gmail.com>
2023-03-28 23:46:34 +0000

    Update printf.c: remove check for whitespaces after %

--- printf.c
+++ printf.c
@@ -9,7 +9,7 @@
  */
 int _printf(const char *format, ...)
 {
-	int j, i, len = 0;
+	int i, len = 0;
 	va_list ap;
 	int (*handler)(va_list);
 
@@ -23,13 +23,6 @@ int _printf(const char *format, ...)
 		{
 			if (format[i + 1] == '\0')
 				return (-1);
-
-			if (format[i + 1] == ' ')
-			{
-				for (j = i + 1; format[j] == ' '; j++)
-					;
-				i = j - 1;
-			}
 
 			handler = get_handler(format[i + 1]);
 
