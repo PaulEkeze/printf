3630933d0a03368b9a12b1d7f319759416b2e8b6
attabadukwabena <attabadukwabena@gmail.com>
2023-03-28 20:04:58 +0000

    Update main.h: Add format_handler structure

--- main.h
+++ main.h
@@ -1,6 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+
+/**
+ * format_handler - sturcture for handling formats in string
+ * @specifier: a specific character or format
+ * @handler: a pointer to a function to handle a format
+ */
+typedef struct format_handler
+{
+	char specifier;
+	int (*handler)(va_list);
+} fhandler;
+
 int _printf(const char *format, ...);
 char *ctos(char ch);
 int printchar(char ch);
