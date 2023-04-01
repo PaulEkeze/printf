57ed2dd823c88c968ca494fe6c02b6a8ebfdff78
attabadukwabena <attabadukwabena@gmail.com>
2023-04-01 01:30:49 +0000

    Move helper functions to parent files

--- binary.c
+++ binary.c
@@ -27,3 +27,32 @@ int bi_handler(va_list ap)
 
 	return (printstr(binary));
 }
+
+/**
+ * get_bi_length - get length of a number after
+ * it's been converted to binary
+ * @decimal: a number in base 10.
+ * Return: length of a binary number.
+ */
+int get_bi_length(unsigned int decimal)
+{
+	if (decimal == 0)
+		return (0);
+
+	return (get_bi_length(decimal / 2) + 1);
+}
+
+/**
+ * dec_to_bi - conver decimal number to binary
+ * @binary: a string to store the binary number
+ * @decimal: a number in base 10.
+ * Return: void.
+ */
+void dec_to_bi(char *binary, unsigned int decimal)
+{
+	if (decimal == 0)
+		return;
+
+	dec_to_bi(binary - 1, decimal / 2);
+	*binary = (decimal % 2) + '0';
+}
--- printer.c
+++ printer.c
@@ -26,3 +26,18 @@ int printstr(char *str)
 
 	return (len);
 }
+
+/**
+ * get_length - gets the number of characters in a string
+ * @str: the string
+ * Return: length of str
+ */
+int get_length(char *str)
+{
+	int len = 0;
+
+	for (; str[len]; len++)
+		;
+
+	return (len);
+}
